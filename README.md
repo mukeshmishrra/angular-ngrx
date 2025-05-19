e95260f9d87091d8fca4025b7bc77933256db0bf
Mukesh Mishra <mukesh@campionsoftware.com>
2025-05-19 12:22:44 +0530

    Starter code added

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# AngularNgrx
+# ngrx-starter
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.2.1.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.0.3.
 
 ## Development server
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-ngrx' title`, () => {
+  it(`should have the 'ngrx-starter' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-ngrx');
+    expect(app.title).toEqual('ngrx-starter');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-ngrx');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ngrx-starter');
   });
 });
